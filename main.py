52b6f77e36ae01e0735e7e7dab85f3d229979d13
pattelu <90059425+pattelu@users.noreply.github.com>
2026-06-04 13:31:27 +0200

    Added scheduler to run every hour and separate code by funtions

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import os
 import datetime
+import time
 import requests
 
 from plyer import notification
 from dotenv import load_dotenv
+from apscheduler.schedulers.background import BackgroundScheduler
 
 load_dotenv()
 
@@ -12,78 +14,98 @@ headers = {
     "Wanikani-Revision": "20170710",
 }
 
-user_level = 0
-available_items = []
-items_to_review = []
 
-now = datetime.datetime.now(datetime.timezone.utc)
+def get_user_level():
+    user_request = "https://api.wanikani.com/v2/user"
+    user_response = requests.get(user_request, headers=headers)
 
-# User data
-user_request = "https://api.wanikani.com/v2/user"
-user_response = requests.get(user_request, headers=headers)
-
-if user_response.status_code == 200:
-    user_data = user_response.json()
-    user_level = user_data['data']['level']
-else:
-    print(f"Error with status code: {user_response.status_code}")
-
-
-# Items data
-assignment_request = "https://api.wanikani.com/v2/assignments"
-
-while assignment_request:
-    assignment_response = requests.get(assignment_request, headers=headers)
-
-    if assignment_response.status_code == 200:
-        assignment_data = assignment_response.json()
-
-        for item in assignment_data['data']:
-            available_at_str = item['data'].get('available_at')
-            item_type = item['data'].get('subject_type')
-
-            if item_type == "kanji" or item_type == "radical":
-                if available_at_str:
-                    available_at = datetime.datetime.fromisoformat(available_at_str.replace('Z', '+00:00'))
-
-                    if available_at <= now:
-                        available_items.append(item['data'].get('subject_id'))
-
-        next_url = assignment_data.get('pages', {}).get('next_url')
-
-        if next_url:
-            assignment_request = next_url
-            params = {}
-        else:
-            assignment_request = None
-
+    if user_response.status_code == 200:
+        user_data = user_response.json()
+        user_level = user_data['data']['level']
+        return user_level
     else:
-        print(f"HTTP error: {assignment_response.status_code}")
-        break
+        return f"Error with status code: {user_response.status_code}"
 
 
-subjects_request = f"https://api.wanikani.com/v2/subjects?levels={user_level}"
-subjects_response = requests.get(subjects_request, headers=headers)
+def get_assignments():
+    assignment_request = "https://api.wanikani.com/v2/assignments"
 
-if subjects_response.status_code == 200:
-    subject_data = subjects_response.json()
+    assignment_items = []
+    now = datetime.datetime.now(datetime.timezone.utc)
 
-    for subject in subject_data['data']:
-        current_level_subject_id = subject['id']
+    while assignment_request:
+        assignment_response = requests.get(assignment_request, headers=headers)
 
-        for item in available_items:
-            if item == current_level_subject_id:
-                items_to_review.append(subject['data'])
+        if assignment_response.status_code == 200:
+            assignment_data = assignment_response.json()
+
+            for assignment in assignment_data['data']:
+                available_at_str = assignment['data'].get('available_at')
+                assignment_type = assignment['data'].get('subject_type')
+
+                if assignment_type == "kanji" or assignment_type == "radical":
+                    if available_at_str:
+                        available_at = datetime.datetime.fromisoformat(available_at_str.replace('Z', '+00:00'))
+
+                        if available_at <= now:
+                            assignment_items.append(assignment['data'].get('subject_id'))
+
+            next_url = assignment_data.get('pages', {}).get('next_url')
+
+            if next_url:
+                assignment_request = next_url
+            else:
+                assignment_request = None
+        else:
+            return f"HTTP error: {assignment_response.status_code}"
+    return assignment_items
 
 
-print(f"User level: {user_level}")
-print(f"Available items: {len(available_items)}")
-print(f"Kanji and Radicals items to review: {len(items_to_review)}")
+def get_items_to_review(user_level, assignment_items):
+    subjects_request = f"https://api.wanikani.com/v2/subjects?levels={user_level}"
+    subjects_response = requests.get(subjects_request, headers=headers)
 
-if len(items_to_review) > 0:
-    notification.notify(
-        title=f"You have {len(items_to_review)} critical items to review.",
-        message=f"頑張れ！",
-        app_name="WaniKani Notify",
-        timeout=0,
-    )
+    items_to_review = []
+
+    if subjects_response.status_code == 200:
+        subject_data = subjects_response.json()
+
+        for subject in subject_data['data']:
+            current_level_subject_id = subject['id']
+
+            for item in assignment_items:
+                if item == current_level_subject_id:
+                    items_to_review.append(subject['data'])
+        return items_to_review
+    else:
+        return f"Error with status code: {subjects_response.status_code}"
+
+def user_notification():
+    print(f"Script run on: {time.strftime('%H:%M:%S')}")
+
+    level = get_user_level()
+    assignments = get_assignments()
+    review = get_items_to_review(level, assignments)
+
+    print(f"Current user level: {level}")
+    print(f"All kanji and radicals to review: {len(assignments)}")
+    print(f"Current level kanji and radicals to review: {len(review)}")
+
+    if len(review) > 0:
+        notification.notify(
+            title=f"Critical review is ready!",
+            message=f"You have {len(review)} critical items to review.",
+            app_name="WaniKani Notify",
+            timeout=0,
+        )
+
+if __name__ == "__main__":
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(user_notification, 'cron', minute=00, second=10)
+    scheduler.start()
+
+    try:
+        while True:
+            time.sleep(1)
+    except(KeyboardInterrupt, SystemExit):
+        scheduler.shutdown()
